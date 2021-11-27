8cce27f2dcea9248770950a5fe955c85a83e3931
linh194093 <linh194093@gmail.com>
2021-11-27 17:16:06 +0700

    up code xuat data

---
+++ Code_trichData/read_minidoc.py
@@ -0,0 +1,9 @@
+import json
+
+#ví dụ với công nghệ
+with open('content_congnghe.json','rb') as f:
+    data = json.load(f)
+    
+print(len(data))
+with open('content_congnghe_mini.json','w', encoding='utf8') as f:
+     json.dump(data[:1000],f,ensure_ascii=False)
